f46f5201f360554dedb406ab69bcb9e62b839c77
my.pham <my.pham@resmed.com.au>
2018-01-07 16:32:40 +1100

    Ground 0 for a GitHub project

---
+++ tests/test_core.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from data_mod import core
+
+import unittest
+
+
+class AdvancedTestSuite(unittest.TestCase):
+    """Advanced test cases."""
+
+    def test_thoughts(self):
+        self.assertIsNone(core.hmm())
+
+
+if __name__ == '__main__':
+    unittest.main()
